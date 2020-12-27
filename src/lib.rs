42e77cc74316a1c6914d8a5d8f9c282988c3932c
nil <nil@kobold.dev>
2020-12-27 01:33:53 -0500

    add channel registration command

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 use serenity::{
    async_trait,
    model::{channel::Message, gateway::Ready},
@@ -9,13 +8,54 @@ use tokio::runtime::Runtime;
 
 struct Handler;
 
+// nil#1337
+const CREATOR_ID: u64 = 124335242176757766;
+
 #[async_trait]
 impl EventHandler for Handler {
-   async fn message(&self, ctx: Context, msg: Message) {
-      if msg.content == "!ping" {
-         if let Err(why) = msg.channel_id.say(&ctx.http, "pong!").await {
-            eprintln!("error sending message: {:?}", why);
+   async fn message(&self, context: Context, message: Message) {
+      if message.content == "kobot lives here" {
+         let channel = match message.channel_id.to_channel(&context).await {
+            Ok(channel) => channel,
+            Err(why) => {
+               eprintln!("error getting channel: {:?}", why);
+               return;
+            }
+         };
+
+         if message.author.id != CREATOR_ID {
+            let dm = message
+               .author
+               .direct_message(&context, |m| {
+                  m.content(format!(
+                     "you're not authorized to control kobot! (from: {})",
+                     channel
+                  ))
+               })
+               .await;
+            if let Err(why) = dm {
+               eprintln!("error sending dm: {:?}", why);
+            }
+
+            return;
          }
+
+         let reply = message
+            .channel_id
+            .say(
+               &context.http,
+               format!("yip! kobot now lives in {} (listen mode enabled)", channel),
+            )
+            .await;
+         if let Err(why) = reply {
+            eprintln!("error sending reply: {:?}", why);
+
+            return;
+         }
+
+         // TODO: register channel (redis?)
+
+         println!("now listening to {} (enabled by {})", channel, message.author)
       }
    }
 
@@ -28,9 +68,7 @@ pub fn run<T: AsRef<str>>(token: T) -> Result<(), Box<dyn std::error::Error>> {
    let mut rt = Runtime::new()?;
 
    rt.block_on(async {
-      let mut client = Client::builder(&token)
-         .event_handler(Handler)
-         .await?;
+      let mut client = Client::builder(&token).event_handler(Handler).await?;
 
       client.start().await?;
 
