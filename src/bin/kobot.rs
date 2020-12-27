e966dcce8ac18847c43d6d17d2119e0783328be7
nil <nil@kobold.dev>
2020-12-26 23:21:20 -0500

    add serenity discord bot framework

--- src/bin/kobot.rs
+++ src/bin/kobot.rs
@@ -2,12 +2,17 @@
 
 use std::env;
 
-pub fn main() {
+fn main() {
    println!("kobot {}", env!("GIT_VERSION"));
 
-   let _args = &env::args_os().collect::<Vec<_>>();
+   //let _args = &env::args_os().collect::<Vec<_>>();
 
-   kobot::run();
+   let token = env::var("DISCORD_TOKEN").expect("discord token");
+
+   if let Err(why) = kobot::run(&token) {
+      eprintln!("error during bot run: {:?}", why);
+      std::process::exit(1);
+   }
 }
 
 // ex:expandtab sw=3 ts=3
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,41 @@
 
-pub fn run() {}
+use serenity::{
+   async_trait,
+   model::{channel::Message, gateway::Ready},
+   prelude::*,
+};
+
+use tokio::runtime::Runtime;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+   async fn message(&self, ctx: Context, msg: Message) {
+      if msg.content == "!ping" {
+         if let Err(why) = msg.channel_id.say(&ctx.http, "pong!").await {
+            eprintln!("error sending message: {:?}", why);
+         }
+      }
+   }
+
+   async fn ready(&self, _: Context, ready: Ready) {
+      println!("{} is connected!", ready.user.name);
+   }
+}
+
+pub fn run<T: AsRef<str>>(token: T) -> Result<(), Box<dyn std::error::Error>> {
+   let mut rt = Runtime::new()?;
+
+   rt.block_on(async {
+      let mut client = Client::builder(&token)
+         .event_handler(Handler)
+         .await?;
+
+      client.start().await?;
+
+      Ok(())
+   })
+}
 
 // ex:expandtab sw=3 ts=3
