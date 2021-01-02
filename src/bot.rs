0f4be90c974b7196a0c2f0c4c1500f3c39b38e96
nil <nil@kobold.dev>
2021-01-02 02:01:55 -0500

    add bot info global data

--- src/bot.rs
+++ src/bot.rs
@@ -1,23 +1,123 @@
+use std::sync::Arc;
+
 use serenity::{
-   model::{id::UserId},
+   async_trait,
    http::Http,
+   model::{channel::Message, gateway::Ready, id::UserId},
+   prelude::*,
 };
 
-pub struct Bot {
+pub struct BotInfo {
    id: UserId,
    owner: UserId,
    token: String,
 }
 
+impl TypeMapKey for BotInfo {
+   type Value = Arc<BotInfo>;
+}
+
+pub struct Bot {
+   info: Arc<BotInfo>,
+}
+
 impl Bot {
    pub async fn new(token: String) -> Result<Bot, Box<dyn std::error::Error>> {
       let http = Http::new_with_token(&token);
-      let info =  http.get_current_application_info().await?;
+      let info = http.get_current_application_info().await?;
 
       Ok(Bot {
-         id: info.id,
-         owner: info.owner.id,
-         token: token,
+         info: Arc::new(BotInfo {
+            id: info.id,
+            owner: info.owner.id,
+            token: token,
+         }),
       })
+   }
+
+   pub async fn connect(&self) -> Result<(), Box<dyn std::error::Error>> {
+      let mut client = Client::builder(&self.info.token)
+         .event_handler(Handler)
+         .await?;
+
+      {
+         let mut data = client.data.write().await;
+         data.insert::<BotInfo>(self.info.clone());
+      }
+
+      client.start().await?;
+
+      Ok(())
+   }
+}
+
+impl TypeMapKey for Bot {
+   type Value = Arc<Bot>;
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+   async fn message(&self, context: Context, message: Message) {
+      if message.content == "kobot lives here" {
+         let channel = match message.channel_id.to_channel(&context).await {
+            Ok(channel) => channel,
+            Err(why) => {
+               eprintln!("error getting channel: {:?}", why);
+               return;
+            }
+         };
+
+         let owner = {
+            let data_read = context.data.read().await;
+            data_read
+               .get::<BotInfo>()
+               .expect("BotInfo in TypeMap")
+               .clone()
+               .owner
+         };
+
+         if message.author.id != owner {
+            let dm = message
+               .author
+               .direct_message(&context, |m| {
+                  m.content(format!(
+                     "you're not authorized to control kobot! (from: {})",
+                     channel
+                  ))
+               })
+               .await;
+            if let Err(why) = dm {
+               eprintln!("error sending dm: {:?}", why);
+            }
+
+            return;
+         }
+
+         let reply = message
+            .channel_id
+            .say(
+               &context.http,
+               format!("yip! kobot now lives in {} (listen mode enabled)", channel),
+            )
+            .await;
+         if let Err(why) = reply {
+            eprintln!("error sending reply: {:?}", why);
+
+            return;
+         }
+
+         // TODO: register channel (redis?)
+
+         println!(
+            "now listening to {} (enabled by {})",
+            channel, message.author.name
+         )
+      }
+   }
+
+   async fn ready(&self, _: Context, ready: Ready) {
+      println!("{} is connected!", ready.user.name);
    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,85 +1,16 @@
-use serenity::{
-   async_trait,
-   model::{channel::Message, gateway::Ready},
-   prelude::*,
-};
-
 use tokio::runtime::Runtime;
 
 use bot::Bot;
 
 mod bot;
 
-// nil#1337
-const CREATOR_ID: u64 = 124335242176757766;
-
-struct Handler;
-
-#[async_trait]
-impl EventHandler for Handler {
-   async fn message(&self, context: Context, message: Message) {
-      if message.content == "kobot lives here" {
-         let channel = match message.channel_id.to_channel(&context).await {
-            Ok(channel) => channel,
-            Err(why) => {
-               eprintln!("error getting channel: {:?}", why);
-               return;
-            }
-         };
-
-         if message.author.id != CREATOR_ID {
-            let dm = message
-               .author
-               .direct_message(&context, |m| {
-                  m.content(format!(
-                     "you're not authorized to control kobot! (from: {})",
-                     channel
-                  ))
-               })
-               .await;
-            if let Err(why) = dm {
-               eprintln!("error sending dm: {:?}", why);
-            }
-
-            return;
-         }
-
-         let reply = message
-            .channel_id
-            .say(
-               &context.http,
-               format!("yip! kobot now lives in {} (listen mode enabled)", channel),
-            )
-            .await;
-         if let Err(why) = reply {
-            eprintln!("error sending reply: {:?}", why);
-
-            return;
-         }
-
-         // TODO: register channel (redis?)
-
-         println!("now listening to {} (enabled by {})", channel, message.author.name)
-      }
-   }
-
-   async fn ready(&self, _: Context, ready: Ready) {
-      println!("{} is connected!", ready.user.name);
-   }
-}
-
 pub fn run<T: AsRef<str>>(token: T) -> Result<(), Box<dyn std::error::Error>> {
    let mut rt = Runtime::new()?;
 
    rt.block_on(async {
-      let _bot = Bot::new(token.as_ref().into()).await?;
+      let bot = Bot::new(token.as_ref().into()).await?;
 
-      let mut client = Client::builder(&token).event_handler(Handler).await?;
-
-      client.start().await?;
-
-
-      Ok(())
+      bot.connect().await
    })
 }
 
